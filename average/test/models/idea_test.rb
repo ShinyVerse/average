978fafa6af3fc559fc49ff6a041beeedf22ee674
Laura Jackson <playeronereadystart@gmail.com>
2018-11-04 13:57:41 +0000

    Add Comments

--- average/test/models/idea_test.rb
+++ average/test/models/idea_test.rb
@@ -4,4 +4,10 @@ class IdeaTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "should not save article without title" do
+    post = Idea.new
+    assert_not post.save,'Saved the article without a title'
+  end
 end
